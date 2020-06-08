84b3bfc7808943b88bca355bffceb5101c365bfe
iona-b <ionabrabender@gmail.com>
2020-06-08 12:01:12 -0700

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -2,7 +2,19 @@
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
-# test code goes here
+iona = Lifter.new("Iona", 150)
+soundarya = Lifter.new("Soundarya", 100)
+danira = Lifter.new("Danira", 300)
+akiko = Lifter.new("Akiko", 160)
+
+twenty_four = Gym.new("24 Hour Fitness")
+golds = Gym.new("Gold Fitness")
+
+m1 = Membership.new(100, iona, twenty_four)
+m2 = Membership.new(100, soundarya, twenty_four)
+m3 = Membership.new(200, danira, golds)
+m4 = Membership.new(200, akiko, golds)
+m5 = Membership.new(200, iona, golds)
 
 binding.pry
 
